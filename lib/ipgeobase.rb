01b5e7a33d9c2e79ac246b3d90de539c685c1766
Pavel Osminin <osperevod@gmail.com>
2024-06-22 23:05:39 +0500

    feat: implelemnt lookup() method

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "addressable/uri"
+require "uri"
+require "net/http"
+require "happymapper"
+require_relative "address"
 
 module Ipgeobase
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.lookup(ip = "8.8.8.8")
+    uri = URI("http://ip-api.com/xml/#{ip}")
+    res = Net::HTTP.get_response(uri)
+    Address.parse(res.body, single: true)
+  end
 end
